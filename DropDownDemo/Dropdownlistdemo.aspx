761ca1242f20c1a2faa07a97ed62f3d9fe4d0c41
rahul shukla <shuklarahul17@gmail.com>
2019-07-09 14:50:17 -0400

    Compete

---
+++ DropDownDemo/Dropdownlistdemo.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Dropdownlistdemo.aspx.cs" Inherits="Dropdownlistdemo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="DropDownList1" runat="server" DataValueField="CityId" DataTextField="CityName">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:DropDownList ID="DropDownList2" runat="server">
+                <asp:ListItem>Male</asp:ListItem>
+                <asp:ListItem Selected="True">Female</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <asp:DropDownList ID="DropDownList3" runat="server">
+            </asp:DropDownList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DropDownDemo/Dropdownlistdemo.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+
+public partial class Dropdownlistdemo : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        //string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+        string CS = @"data source = (LocalDb)\MSSQLLocalDB; database = Sample; Integrated Security = SSPI";
+        using (SqlConnection con = new SqlConnection(CS))
+        {
+            SqlCommand command = new SqlCommand("Select * from tblCity", con);
+            con.Open();
+            DropDownList1.DataSource= command.ExecuteReader();
+            //DropDownList1.DataTextField = "CityName";
+            //DropDownList1.DataValueField = "CityId";
+            //Response.Write("Print" + DropDownList1.ID);
+            DropDownList1.DataBind();
+        }
+        //Bind XML data to dropdown list
+
+        DataSet ds = new DataSet();
+        //Pass the Physical path
+        ds.ReadXml(@"C:\Users\shukl\source\repos\DropDownDemo\DropDownDemo\Country.xml");
+        //If you don't know the physical path use the method Server.MapPath to find the physical path
+        ds.ReadXml(Server.MapPath("Country.xml"));
+
+
+        DropDownList3.DataSource = ds;
+        DropDownList3.DataTextField = "CountryName";
+        DropDownList3.DataValueField = "CountryId";
+        DropDownList3.DataBind();
+
+        //Add will add at the end of the list but we want at the beginning. Use Insert
+        DropDownList3.Items.Insert(0, "Select");
+        
+
+
+    }
+}
